b94bec60040dd9ba3d16bdd0e07cda293493d431
Travis Collins <travis@dreamingwell.com>
2020-08-26 09:40:53 -0500

    Possible fix for orgId

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ data "google_billing_account" "acct" {
 resource "google_project" "project" {
   name            = "datapm TEST"
   project_id      = "datapm-test-terraform"
-  org_id          = "933169977231"
   billing_account = data.google_billing_account.acct.id
   lifecycle {
     prevent_destroy = true
