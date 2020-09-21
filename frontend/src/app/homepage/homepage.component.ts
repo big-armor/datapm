cf49ee35106c1b430b7036e742bd1b5aaff13715
Bleona Gjocaj <bleona@bigarmor.com>
2020-09-21 17:53:25 +0200

    removed unused code

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -8,10 +8,6 @@ import { FormControl } from '@angular/forms';
 })
 export class HomepageComponent implements OnInit {
 
-  toppings = new FormControl();
-
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-
   constructor() { }
 
   ngOnInit(): void {
