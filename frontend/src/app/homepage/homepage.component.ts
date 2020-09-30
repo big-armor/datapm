51b3c12a0c734a42854dbe4778c7382a26e2cf41
Bleona Gjocaj <bleona@bigarmor.com>
2020-09-30 16:10:12 +0200

    feature/ commented the following page

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -9,13 +9,6 @@ export class HomepageComponent implements OnInit {
 
   constructor() { }
 
-  // public routes=[
-  //   {linkName:'trending',url:'/home/',label:"trending"},
-  //   {linkName:'latest',url:'/home/latest',label:"latest"},
-  //   {linkName:'following',url:'/home/following',label:"following"},
-  //   {linkName:'premium',url:'/home/premium',label:"premium"},
-  // ]
-
   ngOnInit(): void {
   }
 
