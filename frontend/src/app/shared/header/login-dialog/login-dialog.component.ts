cfa7399ad4f0208ee8386a5336c6cb6ad2e3c796
Bryan Szendel <bryan@bigarmor.com>
2020-10-06 17:37:31 -0400

    spinner icon shows while awaiting login request

--- frontend/src/app/shared/header/login-dialog/login-dialog.component.ts
+++ frontend/src/app/shared/header/login-dialog/login-dialog.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { LoginGQL } from 'src/generated/graphql';
 enum State {
@@ -8,56 +14,51 @@ enum State {
   AWAITING_RESPONSE,
   INCORRECT_LOGIN,
   LOGGED_IN,
-  LOGIN_ERROR
+  LOGIN_ERROR,
 }
 
 @Component({
   selector: 'app-login-dialog',
   templateUrl: './login-dialog.component.html',
-  styleUrls: ['./login-dialog.component.scss']
+  styleUrls: ['./login-dialog.component.scss'],
 })
 export class LoginDialogComponent implements OnInit {
-
   State = State;
 
   public state = State.LOGGED_OUT;
 
   loginForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('', Validators.required),
   });
 
   constructor(
-    private loginGQL:LoginGQL,
-    private authenticationService:AuthenticationService,
-    private router:Router
-  ) { }
+    private loginGQL: LoginGQL,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-
-    if(this.authenticationService.currentUser != null)
+    if (this.authenticationService.currentUser != null)
       this.state = State.LOGGED_IN;
 
     this.authenticationService.getUserObservable().subscribe((userPromise) => {
-
-      if(userPromise == null) {
+      if (userPromise == null) {
         this.state = State.LOGGED_OUT;
         return;
       }
 
       userPromise.then((user) => {
-        if(user != null)
-          this.state = State.LOGGED_IN;
+        if (user != null) this.state = State.LOGGED_IN;
         else {
-          if(this.state == State.LOGGED_IN)
-            this.state = State.LOGGED_OUT;
+          if (this.state == State.LOGGED_IN) this.state = State.LOGGED_OUT;
         }
-      })
+      });
     });
   }
 
   formSubmit() {
-
     this.state = State.AWAITING_RESPONSE;
 
     this.authenticationService
@@ -65,22 +66,27 @@ export class LoginDialogComponent implements OnInit {
       .then((user) => {
         this.state = State.LOGGED_IN;
 
-        const returnUrl = this.router.parseUrl(this.router.url).queryParams["returnUrl"] || null;
+        const returnUrl =
+          this.router.parseUrl(this.router.url).queryParams['returnUrl'] ||
+          null;
 
-        if(returnUrl) {
+        if (returnUrl) {
+          this.dialog.closeAll();
           this.router.navigate([returnUrl]);
         } else {
+          this.dialog.closeAll();
           this.router.navigate(['/']);
         }
-
-      }).catch((error: any) => {
-        if(error.errors?.find(e => e.extensions.code == "LOGIN_FAILED") != null)
+      })
+      .catch((error: any) => {
+        console.log(error.errors);
+        if (
+          error.errors?.find(
+            (e) => e.extensions.code == 'GRAPHQL_VALIDATION_FAILED'
+          ) != null
+        )
           this.state = State.INCORRECT_LOGIN;
-        else
-          this.state = State.LOGIN_ERROR;
-
+        else this.state = State.LOGIN_ERROR;
       });
-
   }
-
 }
