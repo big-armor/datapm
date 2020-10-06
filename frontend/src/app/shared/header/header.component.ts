a4178dbf09489aa98f05036cf28da632e0cce71d
Bryan Szendel <bryan@bigarmor.com>
2020-10-06 13:13:23 -0400

    sign in/up and My Profile are conditionally hidden based on login

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -5,24 +5,46 @@ import { Router } from '@angular/router';
 import { User } from 'src/generated/graphql';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { SignUpDialogComponent } from './sign-up-dialog/sign-up-dialog.component';
+import { AuthenticationService } from '../../services/authentication.service';
 
+enum State {
+  INIT,
+  LOADING,
+  ERROR,
+  SUCCESS,
+  ERROR_NOT_UNIQUE,
+}
 @Component({
   selector: 'sd-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  state = State.INIT;
 
-  currentUser:User;
-  searchFormGroup:FormGroup;
+  currentUser: User;
+  searchFormGroup: FormGroup;
 
   constructor(
     public dialog: MatDialog,
-    private router: Router) {}
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
+    this.authenticationService.getUserObservable().subscribe((u) => {
+      if (u == null) {
+        return;
+      }
+
+      u.then((user) => {
+        this.currentUser = user;
+        this.state = State.SUCCESS;
+      }).catch((error) => (this.state = State.ERROR));
+    });
+
     this.searchFormGroup = new FormGroup({
-      search: new FormControl('')
+      search: new FormControl(''),
     });
   }
 
@@ -36,16 +58,14 @@ export class HeaderComponent implements OnInit {
 
   goToSearch() {
     this.router.navigate(['/search']);
-
   }
 
   search() {
     const query = this.searchFormGroup.value.search;
-    this.router.navigate(['/search',{q: query}])
+    this.router.navigate(['/search', { q: query }]);
   }
 
   goHome() {
-    this.router.navigate(['/latest'])
+    this.router.navigate(['/latest']);
   }
-
 }
