976f6d2c6df0288b3ee62c229e526de95c0d3679
ErmalFerati <ermalferati@gmail.com>
2020-10-08 21:49:22 +0200

    bugfix/DPM - Add first user as the package and collection creator

--- backend/src/migration/1602010013926-CollectionCreator.ts
+++ backend/src/migration/1602010013926-CollectionCreator.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 const sql = `
 DELETE FROM public.collection;
 ALTER TABLE public.collection ADD COLUMN creator_id BIGINT NOT NULL; 
-
+UPDATE public.collection SET creator_id = (SELECT id FROM public.user WHERE 1=1 LIMIT 1);
 ALTER TABLE ONLY public.collection ADD CONSTRAINT "FK_collection_user" FOREIGN KEY (creator_id) REFERENCES public.user(id) ON DELETE CASCADE;
 `
 export class CollectionCreator1602010013926 implements MigrationInterface {
