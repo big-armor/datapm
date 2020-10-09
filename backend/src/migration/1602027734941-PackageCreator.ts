c43664ed1628ef5bc74235ec76082ce7b7f487be
Travis Collins <travis@dreamingwell.com>
2020-10-09 12:00:04 -0400

    Foreign key delete on cascade for package versions

--- backend/src/migration/1602027734941-PackageCreator.ts
+++ backend/src/migration/1602027734941-PackageCreator.ts
@@ -1,21 +1,24 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
-
-
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 const sql = `
+
+
+alter table public.version
+drop constraint FK_2bc1bd51e8ca9ed656bf6409e32,
+add constraint FK_2bc1bd51e8ca9ed656bf6409e32
+   foreign key (package_id)
+   references public.package(id)
+   on delete cascade;
+
 DELETE FROM public.package;
-ALTER TABLE public.package ADD COLUMN creator_id BIGINT NOT NULL;
-UPDATE public.package SET creator_id = (SELECT id FROM public.user WHERE 1=1 LIMIT 1);
+ALTER TABLE public.package ADD COLUMN creator_id BIGINT  NOT NULL;
 ALTER TABLE ONLY public.package ADD CONSTRAINT "FK_package_creator_user" FOREIGN KEY (creator_id) REFERENCES public.user(id) ON DELETE CASCADE;
 
-`
+`;
 export class PackageCreator1602027734941 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		queryRunner.query(sql);
+	}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(sql);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {}
 }
