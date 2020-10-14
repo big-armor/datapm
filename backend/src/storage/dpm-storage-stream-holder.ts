509e6962264acbf5c2e7905810aba6f0c6678b27
ErmalFerati <ermalferati@gmail.com>
2020-10-14 23:38:50 +0200

    chore/DPM - Reduce tab width size to 2 in prettier config

--- backend/src/storage/dpm-storage-stream-holder.ts
+++ backend/src/storage/dpm-storage-stream-holder.ts
@@ -1,55 +1,55 @@
 import { Readable, Stream, Writable } from "stream";
 
 export class DpmStorageStreamHolder {
-	private readonly OPEN_READ_STREAMS: Readable[] = [];
-	private readonly OPEN_WRITE_STREAMS: Writable[] = [];
+    private readonly OPEN_READ_STREAMS: Readable[] = [];
+    private readonly OPEN_WRITE_STREAMS: Writable[] = [];
 
-	public resolveReadStream(stream: Readable): Promise<Stream> {
-		this.registerReadStream(stream);
-		return Promise.resolve(stream);
-	}
+    public resolveReadStream(stream: Readable): Promise<Stream> {
+        this.registerReadStream(stream);
+        return Promise.resolve(stream);
+    }
 
-	public copyToStream(dataStream: Stream, targetStream: Writable): Promise<void> {
-		this.registerWriteStream(targetStream);
-		return new Promise<void>((resolve, reject) => {
-			dataStream.on("data", (data) => targetStream.write(data));
-			dataStream.on("end", () => {
-				targetStream.end();
-				resolve();
-			});
-			dataStream.on("error", (error) => {
-				targetStream.destroy();
-				reject(error);
-			});
-		});
-	}
+    public copyToStream(dataStream: Stream, targetStream: Writable): Promise<void> {
+        this.registerWriteStream(targetStream);
+        return new Promise<void>((resolve, reject) => {
+            dataStream.on("data", (data) => targetStream.write(data));
+            dataStream.on("end", () => {
+                targetStream.end();
+                resolve();
+            });
+            dataStream.on("error", (error) => {
+                targetStream.destroy();
+                reject(error);
+            });
+        });
+    }
 
-	public destroyOpenStreams(): boolean {
-		try {
-			if (this.OPEN_READ_STREAMS.length) {
-				this.OPEN_READ_STREAMS.forEach((stream) => stream.destroy());
-			}
+    public destroyOpenStreams(): boolean {
+        try {
+            if (this.OPEN_READ_STREAMS.length) {
+                this.OPEN_READ_STREAMS.forEach((stream) => stream.destroy());
+            }
 
-			if (this.OPEN_WRITE_STREAMS.length) {
-				this.OPEN_WRITE_STREAMS.forEach((stream) => stream.destroy());
-			}
-			return true;
-		} catch (e) {
-			console.error("Could not flush read and write streams", e);
-			return false;
-		}
-	}
+            if (this.OPEN_WRITE_STREAMS.length) {
+                this.OPEN_WRITE_STREAMS.forEach((stream) => stream.destroy());
+            }
+            return true;
+        } catch (e) {
+            console.error("Could not flush read and write streams", e);
+            return false;
+        }
+    }
 
-	private registerReadStream(stream: Readable): void {
-		this.registerStream(stream, this.OPEN_READ_STREAMS);
-	}
+    private registerReadStream(stream: Readable): void {
+        this.registerStream(stream, this.OPEN_READ_STREAMS);
+    }
 
-	private registerWriteStream(stream: Writable): void {
-		this.registerStream(stream, this.OPEN_WRITE_STREAMS);
-	}
+    private registerWriteStream(stream: Writable): void {
+        this.registerStream(stream, this.OPEN_WRITE_STREAMS);
+    }
 
-	private registerStream(stream: Stream, streamCollection: Stream[]): void {
-		const streamIndex = streamCollection.push(stream) - 1;
-		stream.on("close", () => streamCollection.splice(streamIndex, 1));
-	}
+    private registerStream(stream: Stream, streamCollection: Stream[]): void {
+        const streamIndex = streamCollection.push(stream) - 1;
+        stream.on("close", () => streamCollection.splice(streamIndex, 1));
+    }
 }
