e3e73da33a387dc825f70b671c2503da373ab0f6
Travis Collins <travis@dreamingwell.com>
2020-10-15 10:12:18 -0400

    Added document node generation

--- test/setup.ts
+++ test/setup.ts
@@ -1,24 +1,30 @@
-import  * as path from 'path';
-import { DockerComposeEnvironment, StartedDockerComposeEnvironment, Wait } from 'testcontainers';
-import { expect } from 'chai';
-import { ApolloClient,ApolloQueryResult, InMemoryCache, NormalizedCacheObject, gql, HttpLink } from '@apollo/client/core';
-import fetch from 'cross-fetch';
-import { Catalog, Maybe, MyCatalogsQuery, User } from './registry-client'
+import * as path from "path";
+import { DockerComposeEnvironment, StartedDockerComposeEnvironment, Wait } from "testcontainers";
+import { expect } from "chai";
+import {
+	ApolloClient,
+	ApolloQueryResult,
+	InMemoryCache,
+	NormalizedCacheObject,
+	gql,
+	HttpLink
+} from "@apollo/client/core";
+import fetch from "cross-fetch";
+import { Catalog, Maybe, MyCatalogsQuery, User, MyCatalogsDocument } from "./registry-client";
 
-let environment:StartedDockerComposeEnvironment;
+let environment: StartedDockerComposeEnvironment;
 
-before(async ()=> {
-  const composeFilePath = path.resolve("docker/");
-  const composeFile = "docker-compose.yml";
+before(async () => {
+	const composeFilePath = path.resolve("docker/");
+	const composeFile = "docker-compose.yml";
 
-  console.log(composeFilePath + "/" + composeFile);
-  environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
-    .withWaitStrategy("postgres",Wait.forLogMessage("listening on IPv4 address \"0.0.0.0\", port 5432"))
-    .withWaitStrategy("datapm-registry",Wait.forLogMessage("🚀 Server ready at http://localhost:4000"))
-    .up();
+	console.log(composeFilePath + "/" + composeFile);
+	environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
+		.withWaitStrategy("postgres", Wait.forLogMessage('listening on IPv4 address "0.0.0.0", port 5432'))
+		.withWaitStrategy("datapm-registry", Wait.forLogMessage("🚀 Server ready at http://localhost:4000"))
+		.up();
 
-
-    /*
+	/*
 const registryContainer = environment.getContainer("datapm-registry");
   const stream = await registryContainer.logs();
     stream
@@ -27,182 +33,141 @@ const registryContainer = environment.getContainer("datapm-registry");
         .on("end", () => console.log("Stream closed"));
   */
 
-  // The "Wait" strategy above doesn't appear to actually wait for the container to start
-  await new Promise((r)=>{
-    setTimeout(async() => { 
-      r()
-    },10000)
-  });
-
-  
-  
-
-})
-
-
-after(async () => {
-
-  if(environment)
-    environment.stop();
-  
+	// The "Wait" strategy above doesn't appear to actually wait for the container to start
+	await new Promise((r) => {
+		setTimeout(async () => {
+			r();
+		}, 10000);
+	});
 });
 
+after(async () => {
+	if (environment) environment.stop();
+});
 
-function createAnonymousClient(){
-  return new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({ uri: 'http://localhost:4000/graphql', fetch })
-  });
+function createAnonymousClient() {
+	return new ApolloClient({
+		cache: new InMemoryCache(),
+		link: new HttpLink({ uri: "http://localhost:4000/graphql", fetch })
+	});
 }
-
 
 /** creates a new user and returns an apollo client for their session */
-async function createUser(firstName:string,lastName:string,username:string,emailAddress:string,password:string):Promise<ApolloClient<NormalizedCacheObject>> {
-  let createFailed = false;
+async function createUser(
+	firstName: string,
+	lastName: string,
+	username: string,
+	emailAddress: string,
+	password: string
+): Promise<ApolloClient<NormalizedCacheObject>> {
+	let createFailed = false;
 
-  return await new Promise((resolve,reject) => {
+	return await new Promise((resolve, reject) => {
+		let client = createAnonymousClient();
 
-    let client = createAnonymousClient();
+		client
+			.mutate({
+				mutation: gql`
+					mutation CreateMe($value: CreateUserInput!) {
+						createMe(value: $value)
+					}
+				`,
+				variables: {
+					value: {
+						firstName: firstName,
+						lastName: lastName,
+						username: username,
+						emailAddress: emailAddress,
+						password: password
+					}
+				}
+			})
+			.catch((error) => {
+				console.error(JSON.stringify(error, null, 1));
+				createFailed = true;
+			})
+			.then((result) => {
+				expect(result != null).equal(true);
+				expect(result!["data"].createMe != null).equal(true);
+				expect(createFailed, "User create should not fail").equal(false);
 
-    client
-    .mutate({
-      mutation: gql`
-        mutation CreateMe($value: CreateUserInput!) {
-          createMe(value: $value)
-        }
-      `,
-      variables: {
-        "value": {
-          "firstName": firstName,
-          "lastName": lastName,
-          "username": username,
-          "emailAddress": emailAddress,
-          "password": password
-        }
-      }
-    })
-    .catch(error => {
-      console.error(JSON.stringify(error,null,1));
-      createFailed = true;
-    })
-    .then(result =>  {
-      expect(result != null).equal(true);
-      expect(result!["data"].createMe != null).equal(true);
-      expect(createFailed,"User create should not fail").equal(false);
+				let token = result!["data"].createMe;
 
+				let client = new ApolloClient({
+					cache: new InMemoryCache(),
+					link: new HttpLink({
+						uri: "http://localhost:4000/graphql",
+						headers: {
+							Accept: "charset=utf-8",
+							Authorization: "Bearer " + token
+						},
+						fetch
+					})
+				});
 
-      let token = result!["data"].createMe;
-
-
-      let client = new ApolloClient({
-        cache: new InMemoryCache(),
-        link: new HttpLink({ 
-          uri: 'http://localhost:4000/graphql', 
-          headers: {
-            Accept: 'charset=utf-8',
-            Authorization: "Bearer " + token
-          },
-          fetch }),
-        
-      });
-
-      resolve(client);
-    });
-
-    
-  });
-
-  
+				resolve(client);
+			});
+	});
 }
 
-
 describe("Authentication Tests", async () => {
+	let anonymousClient: ApolloClient<NormalizedCacheObject> = createAnonymousClient();
+	let userAClient: ApolloClient<NormalizedCacheObject>;
+	let userBClient: ApolloClient<NormalizedCacheObject>;
 
+	before(async () => {
+		expect(anonymousClient).to.exist;
+	});
 
-  let anonymousClient:ApolloClient<NormalizedCacheObject> = createAnonymousClient();
-  let userAClient:ApolloClient<NormalizedCacheObject>;
-  let userBClient:ApolloClient<NormalizedCacheObject>;
-  
-  before(async () => {
+	it("Login for incorrect user should fail", async () => {
+		let loginFailed = false;
 
+		anonymousClient
+			.mutate({
+				mutation: gql`
+					mutation Login($username: String!, $password: String!) {
+						login(username: $username, password: $password)
+					}
+				`,
+				variables: {
+					username: "test",
+					password: "test1234"
+				}
+			})
+			.catch((error) => {
+				console.log("Succes: Login failed when it should");
+				loginFailed = true;
+			})
+			.then((result) => {
+				expect(loginFailed, "No user should be returned, because they are not loggedin").equal(true);
+			});
+	});
 
-     
-    expect(anonymousClient).to.exist;
+	it("Create users A & B", async function () {
+		userAClient = await createUser("FirstA", "LastA", "testA", "testA@test.datapm.io", "passwordA");
+		userBClient = await createUser("FirstB", "LastB", "testB", "testB@test.datapm.io", "passwordB");
+		expect(userAClient).to.exist;
+		expect(userBClient).to.exist;
+	});
 
+	it("Get catalogs for user A & B", async function () {
+		return userAClient
+			.query<MyCatalogsQuery>({
+				query: MyCatalogsDocument
+			})
+			.catch((error) => {
+				console.error(error);
+				expect(true, "getting user catalogs failed").equal(false);
+			})
+			.then((result: ApolloQueryResult<MyCatalogsQuery>) => {
+				expect(result).to.exist;
 
- 
-  });
+				let catalogs = result!.data.myCatalogs;
 
-  it("login for incorrect user should fail", async () => {
+				expect(catalogs.length).equal(1);
 
-    let loginFailed = false;
-
-    anonymousClient
-    .mutate({
-      mutation: gql`
-        mutation Login($username:String!,$password:String!) {
-          login(username:$username, password:$password)
-        }
-      `,
-      variables: {
-        "username": "test",
-        "password": "test1234"
-      }
-    })
-    .catch(error => {
-      console.log("Succes: Login failed when it should")
-      loginFailed = true;
-    })
-    .then(result =>  {
-      expect(loginFailed,"No user should be returned, because they are not loggedin").equal(true);
-    });
-  });
-
-  it("create users A & B", async function() {
-    userAClient = await createUser("FirstA","LastA","testA","testA@test.datapm.io","passwordA");
-    userBClient = await createUser("FirstB","LastB","testB","testB@test.datapm.io","passwordB");
-    expect(userAClient).to.exist;
-    expect(userBClient).to.exist;
-  });
-
-  it("Get catalogs for user A & B", async function() {
-
-
-    return userAClient.query<MyCatalogsQuery>({
-      query: gql`
-      query MyCatalogs {
-        myCatalogs {
-          identifier {
-            registryHostname
-            registryPort
-            catalogSlug
-          }
-          displayName
-          description
-          website
-          isPublic
-        }
-      }
-      `
-    }).catch(error => {
-      console.error(error);
-      expect(true,"getting user catalogs failed").equal(false);
-    }).then((result:ApolloQueryResult<MyCatalogsQuery>) => {
-
-      expect(result).to.exist;
-
-      let catalogs = result!.data.myCatalogs;
-
-      expect(catalogs.length).equal(1);
-
-      expect(catalogs[0].identifier.catalogSlug == "testA");
-      expect(catalogs[0].isPublic).equal(false);
-
-    })
-
-  });
-
-
-
-
+				expect(catalogs[0].identifier.catalogSlug == "testA");
+				expect(catalogs[0].isPublic).equal(false);
+			});
+	});
 });
