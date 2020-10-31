2ee8eedabc1ae5f7f8a5a4465eb0dde5b56b0c94
fullstackdevs14 <fullstackdevs@outlook.com>
2020-10-31 15:18:37 -0700

    fix unit tests

--- frontend/src/app/shared/header/forgot-password-dialog/forgot-password-dialog.component.spec.ts
+++ frontend/src/app/shared/header/forgot-password-dialog/forgot-password-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { ForgotPasswordDialogComponent } from "./forgot-password-dialog.component";
 
@@ -8,7 +9,8 @@ describe("ForgotPasswordDialogComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ForgotPasswordDialogComponent]
+            declarations: [ForgotPasswordDialogComponent],
+            imports: [FormsModule, ReactiveFormsModule]
         }).compileComponents();
     }));
 
--- frontend/src/app/verify-email/verify-email.module.ts
+++ frontend/src/app/verify-email/verify-email.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { VerifyEmailRoutingModule } from "./verify-email-routing.module";
 import { VerifyEmailComponent } from "./verify-email/verify-email.component";
 import { FormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
     declarations: [VerifyEmailComponent],
-    imports: [CommonModule, FormsModule, VerifyEmailRoutingModule]
+    imports: [CommonModule, FormsModule, VerifyEmailRoutingModule, MatDialogModule]
 })
 export class VerifyEmailModule {}
