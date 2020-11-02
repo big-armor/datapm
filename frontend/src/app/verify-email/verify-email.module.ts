30ae2c3ae2b17410efba4040ed06e809a8fb9dc0
fullstackdevs14 <fullstackdevs@outlook.com>
2020-11-02 11:58:10 -0800

    update email validation flow

--- frontend/src/app/verify-email/verify-email.module.ts
+++ frontend/src/app/verify-email/verify-email.module.ts
@@ -5,9 +5,10 @@ import { VerifyEmailRoutingModule } from "./verify-email-routing.module";
 import { VerifyEmailComponent } from "./verify-email/verify-email.component";
 import { FormsModule } from "@angular/forms";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     declarations: [VerifyEmailComponent],
-    imports: [CommonModule, FormsModule, VerifyEmailRoutingModule, MatDialogModule]
+    imports: [CommonModule, FormsModule, VerifyEmailRoutingModule, MatDialogModule, MatIconModule]
 })
 export class VerifyEmailModule {}
