571b2c3f041ef705f60a672b551b79cb1103f2c8
fullstackdevs14 <fullstackdevs@outlook.com>
2020-11-06 11:14:46 -0800

    fix unit tests

--- frontend/src/app/my-account/catalogs/catalogs.component.spec.ts
+++ frontend/src/app/my-account/catalogs/catalogs.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { ApolloTestingModule } from "apollo-angular/testing";
 import { RouterModule } from "@angular/router";
 import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { CatalogsComponent } from "./catalogs.component";
@@ -17,6 +18,7 @@ describe("CatalogsComponent", () => {
             imports: [
                 RouterModule.forRoot([]),
                 MatCardModule,
+                MatDialogModule,
                 ApolloTestingModule,
                 RouterTestingModule,
                 FormsModule,
--- frontend/src/app/my-account/delete-confirmation/delete-confirmation.component.spec.ts
+++ frontend/src/app/my-account/delete-confirmation/delete-confirmation.component.spec.ts
@@ -1,4 +1,9 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 import { DeleteConfirmationComponent } from "./delete-confirmation.component";
 
@@ -8,7 +13,18 @@ describe("DeleteConfirmationComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [DeleteConfirmationComponent]
+            declarations: [DeleteConfirmationComponent],
+            imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, NoopAnimationsModule],
+            providers: [
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {}
+                },
+                {
+                    provide: MatDialogRef,
+                    useValue: {}
+                }
+            ]
         }).compileComponents();
     }));
 
