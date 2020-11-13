9c47a16e19cf2e882763d93fb159d3cd5084f36a
Travis Collins <travis@dreamingwell.com>
2020-11-12 23:49:06 -0500

    Fix for CUSTOM_ELEMENTS_SCHEMA and duplicated SharedModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -19,7 +19,6 @@ import { SharedModule } from "./shared/shared.module";
         HttpClientModule,
         ReactiveFormsModule,
         FormsModule,
-        SharedModule,
         SharedModule
     ],
     providers: [],
