a2c910f3b66d77f01d1e93f2dacbb4328f4ced01
essxiv <essxiv@gmail.com>
2020-11-13 19:17:31 -0500

    Revert activity component

--- frontend/src/app/my-account/activity/activity.component.ts
+++ frontend/src/app/my-account/activity/activity.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { MyCollectionsGQL } from "src/generated/graphql";
-
-class MyCollections {
-    allCollections: any;
-}
 
 @Component({
     selector: "me-activity",
@@ -11,21 +6,7 @@ class MyCollections {
     styleUrls: ["./activity.component.scss"]
 })
 export class ActivityComponent implements OnInit {
-    public collections: MyCollections[] = [];
-    constructor(private myCollections: MyCollectionsGQL) {}
+    constructor() {}
 
-    ngOnInit(): void {
-        this.loadMyCollections();
-    }
-
-    private loadMyCollections(): void {
-        // Need to set a dynamic limit for future / pagination
-        this.myCollections.fetch({ offSet: 0, limit: 5 }).subscribe((a) => {
-            this.collections = a.data.myCollections.collections.map((p) => {
-                return {
-                    allCollections: p
-                };
-            });
-        });
-    }
+    ngOnInit(): void {}
 }
