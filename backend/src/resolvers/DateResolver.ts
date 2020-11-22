6f05bd8c9f88b41b551424579caeb647dfa04a14
Travis Collins <travis@dreamingwell.com>
2020-11-21 19:12:23 -0500

    Fix for build and small package details page adjustment

--- backend/src/resolvers/DateResolver.ts
+++ backend/src/resolvers/DateResolver.ts
@@ -3,22 +3,21 @@ import { GraphQLScalarType } from "graphql";
 
 export class DateResolver extends GraphQLScalarType {
     constructor() {
-        
-        super(        {
-            name: 'Date',
-            description: 'Date custom scalar type',
+        super({
+            name: "Date",
+            description: "Date custom scalar type",
             parseValue(value) {
-              return new Date(value); // value from the client
+                return new Date(value); // value from the client
             },
-            serialize(value) {
-              return value.getTime(); // value sent to the client
+            serialize(value: Date) {
+                return value.toISOString(); // value sent to the client
             },
             parseLiteral(ast) {
-              if (ast.kind === Kind.INT) {
-                return parseInt(ast.value, 10); // ast value is always in string format
-              }
-              return null;
-            })
-
-    );
+                if (ast.kind === Kind.INT) {
+                    return ast.value; // ast value is always in string format
+                }
+                return null;
+            }
+        });
+    }
 }
