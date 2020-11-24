0511e8ead1998024c2f8ad5c84f95abba85ccb6c
essxiv <essxiv@gmail.com>
2020-11-24 12:17:26 -0500

    Revised all test-forgot-password tests to use email regex token parsed, which now just inserts into RecoverMyPassword mutation without any email or other identifier than the UUID itself

--- backend/test/integration/test-forgot-password.ts
+++ backend/test/integration/test-forgot-password.ts
@@ -29,7 +29,6 @@ describe("Forgot Password Tests", async () => {
                 emailAddress: "does-not-exist-user@test.datapm.io"
             }
         });
-
         expect(user.data!.forgotMyPassword).equal(null);
     });
 
@@ -42,14 +41,12 @@ describe("Forgot Password Tests", async () => {
                 r(email);
             });
         });
-
         await userAClient.mutate({
             mutation: ForgotMyPasswordDocument,
             variables: {
                 emailAddress: "forgotpasswordA-user@test.datapm.io"
             }
         });
-
         await verifyEmailPromise.then((email) => {
             expect(email.html).to.not.contain("{{registry_name}}");
             expect(email.html).to.not.contain("{{registry_url}}");
@@ -60,86 +57,46 @@ describe("Forgot Password Tests", async () => {
             expect(email.text).to.not.contain("{{token}}");
             expect(email.text).to.not.contain("{{");
         });
-
         return await verifyEmailPromise;
     });
 
-    it("Email sent actually contains a token", async function () {
+    it("Email sent actually contains a token and recovery token is valid", async function () {
         const verifyEmailPromise = new Promise<any>((r) => {
             let subscription = mailObservable.subscribe((email) => {
                 subscription.unsubscribe();
                 r(email);
             });
         });
-        const user = await userAClient.mutate({
+        await userAClient.mutate({
             mutation: ForgotMyPasswordDocument,
             variables: {
                 emailAddress: "forgotpasswordA-user@test.datapm.io"
             }
         });
-        const forgotPasswordToken = user.data!.forgotMyPassword.passwordRecoveryToken;
+
         await verifyEmailPromise.then((email) => {
-            expect(email.html).to.contain(forgotPasswordToken);
-            expect(email.text).to.contain(forgotPasswordToken);
+            let token;
+            const regex = /[a-zA-z0-9-]/g;
+            const emailForgotToken = (email.text as String).match(/\?token=([a-zA-z0-9-]+)/);
+            if (emailForgotToken) token = regex.test(emailForgotToken[1]);
+            expect(emailForgotToken != null).true;
+            expect(token).to.equal(true);
         });
-        return await verifyEmailPromise;
-    });
-
-    it("User password recovery token is valid", async function () {
-        const verifyEmailPromise = new Promise<any>((r) => {
-            let subscription = mailObservable.subscribe((email) => {
-                subscription.unsubscribe();
-                r(email);
-            });
-        });
-
-        const forgotUser = await userAClient.mutate({
-            mutation: ForgotMyPasswordDocument,
-            variables: {
-                emailAddress: "forgotpasswordA-user@test.datapm.io"
-            }
-        });
-        await verifyEmailPromise;
-        const regex = /[a-zA-z0-9-]/g;
-        const forgotPasswordToken = forgotUser.data!.forgotMyPassword.passwordRecoveryToken;
-        const passwordRegExTest = regex.test(forgotPasswordToken);
-        expect(passwordRegExTest).to.equal(true);
         return await verifyEmailPromise;
     });
 
     it("Should check if RecoverMyPassword TOKEN is not valid", async function () {
-        const verifyEmailPromise = new Promise<any>((r) => {
-            let subscription = mailObservable.subscribe((email) => {
-                subscription.unsubscribe();
-                r(email);
-            });
-        });
-
-        const forgotPasswordUser = await userAClient.mutate({
-            mutation: ForgotMyPasswordDocument,
-            variables: {
-                emailAddress: "forgotpasswordA-user@test.datapm.io"
-            }
-        });
-
-        const wrongToken = uuid();
-        const email = forgotPasswordUser.data?.forgotMyPassword.emailAddress;
-
         const recoverMyPasswordUser = await userAClient.mutate({
             mutation: RecoverMyPasswordDocument,
             variables: {
                 value: {
-                    emailAddress: email,
-                    token: wrongToken,
+                    token: "definitely_the_wrong-to%@n",
                     newPassword: "blackAndWhite!"
                 }
             }
         });
-
-        await verifyEmailPromise;
-        expect(recoverMyPasswordUser.errors != null).true;
+        expect(recoverMyPasswordUser.errors! != null).true;
         expect(recoverMyPasswordUser.errors![0].message).to.equal("TOKEN_NOT_VALID");
-        return await verifyEmailPromise;
     });
 
     it("Should use the token captured in the fortPassword integration test to attempt a correct validation", async function () {
@@ -149,27 +106,30 @@ describe("Forgot Password Tests", async () => {
                 r(email);
             });
         });
-        const forgotPasswordUser = await userAClient.mutate({
+        await userAClient.mutate({
             mutation: ForgotMyPasswordDocument,
             variables: {
                 emailAddress: "forgotpasswordA-user@test.datapm.io"
             }
         });
-        const email = forgotPasswordUser.data?.forgotMyPassword.emailAddress;
-        const token = forgotPasswordUser.data?.forgotMyPassword.passwordRecoveryToken;
-        let recoverMyPasswordUser = await userAClient.mutate({
+
+        const token = await verifyEmailPromise.then((email) => {
+            let token;
+            const emailForgotToken = (email.text as String).match(/\?token=([a-zA-z0-9-]+)/);
+            if (emailForgotToken) token = emailForgotToken[1];
+            return token;
+        });
+
+        const correctRecoveryToken = await userAClient.mutate({
             mutation: RecoverMyPasswordDocument,
             variables: {
                 value: {
-                    emailAddress: email,
                     token: token,
                     newPassword: "blackAndWhite!"
                 }
             }
         });
-        await verifyEmailPromise;
-        expect(recoverMyPasswordUser.errors == null).true;
-        expect(recoverMyPasswordUser.data?.recoverMyPassword.passwordRecoveryToken).to.equal(token);
+        expect(correctRecoveryToken.errors! == null).true;
         return await verifyEmailPromise;
     });
 
@@ -180,30 +140,33 @@ describe("Forgot Password Tests", async () => {
                 r(email);
             });
         });
-
-        const forgotPasswordUser = await userAClient.mutate({
+        await userAClient.mutate({
             mutation: ForgotMyPasswordDocument,
             variables: {
                 emailAddress: "forgotpasswordA-user@test.datapm.io"
             }
         });
-        const email = forgotPasswordUser.data?.forgotMyPassword.emailAddress;
-        const token = forgotPasswordUser.data?.forgotMyPassword.passwordRecoveryToken;
-        const username = forgotPasswordUser.data?.forgotMyPassword.username;
-        const changeUserPassword = await userAClient.mutate({
+        const token = await verifyEmailPromise.then((email) => {
+            let token;
+            const emailForgotToken = (email.text as String).match(/\?token=([a-zA-z0-9-]+)/);
+            if (emailForgotToken) token = emailForgotToken[1];
+            return token;
+        });
+
+        await userAClient.mutate({
             mutation: RecoverMyPasswordDocument,
             variables: {
                 value: {
-                    emailAddress: email,
                     token: token,
                     newPassword: "blueAndYellow!"
                 }
             }
         });
+
         let loginWithNewPassword = await userAClient.mutate({
             mutation: LoginDocument,
             variables: {
-                username: username,
+                username: "forgotpassword-user",
                 password: "blueAndYellow!"
             }
         });
@@ -220,29 +183,33 @@ describe("Forgot Password Tests", async () => {
                 r(email);
             });
         });
-        const forgotPasswordUser = await userAClient.mutate({
+        await userAClient.mutate({
             mutation: ForgotMyPasswordDocument,
             variables: {
                 emailAddress: "forgotpasswordA-user@test.datapm.io"
             }
         });
-        const email = forgotPasswordUser.data?.forgotMyPassword.emailAddress;
-        const token = forgotPasswordUser.data?.forgotMyPassword.passwordRecoveryToken;
-        const username = forgotPasswordUser.data?.forgotMyPassword.username;
-        const changeUserPassword = await userAClient.mutate({
+        const token = await verifyEmailPromise.then((email) => {
+            let token;
+            const emailForgotToken = (email.text as String).match(/\?token=([a-zA-z0-9-]+)/);
+            if (emailForgotToken) token = emailForgotToken[1];
+            return token;
+        });
+
+        await userAClient.mutate({
             mutation: RecoverMyPasswordDocument,
             variables: {
                 value: {
-                    emailAddress: email,
                     token: token,
                     newPassword: "blueAndYellow!"
                 }
             }
         });
+
         const loginWithNewPassword = await userAClient.mutate({
             mutation: LoginDocument,
             variables: {
-                username: username,
+                username: "forgotpassword-user",
                 password: "passwordA2!"
             }
         });
