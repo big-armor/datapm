eca3f64b7e8d7064743c5df45bf215257286c417
fullstackdevs14 <fullstackdevs@outlook.com>
2020-12-18 01:04:53 -0800

    WIP: Adding catalog details page

--- frontend/src/app/catalog/catalog.module.ts
+++ frontend/src/app/catalog/catalog.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatTabsModule } from "@angular/material/tabs";
+import { SharedModule } from "../shared/shared.module";
 
 import { CatalogRoutingModule } from "./catalog-routing.module";
 import { CatalogDetailsComponent } from "./catalog-details/catalog-details.component";
@@ -8,6 +11,6 @@ import { CatalogComponent } from "./catalog/catalog.component";
 
 @NgModule({
     declarations: [CatalogDetailsComponent, UserDetailsComponent, CatalogComponent],
-    imports: [CommonModule, CatalogRoutingModule]
+    imports: [CommonModule, CatalogRoutingModule, MatProgressSpinnerModule, MatTabsModule, SharedModule]
 })
 export class CatalogModule {}
