4fb5249dd1df261d163aa517bad05d249cd2ab06
fullstackdevs14 <fullstackdevs@outlook.com>
2020-12-18 06:44:46 -0800

    Add header to user details page

--- frontend/src/app/catalog/user-details/user-details.component.ts
+++ frontend/src/app/catalog/user-details/user-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { PageState } from "src/app/models/page-state";
+import { AuthenticationService } from "src/app/services/authentication.service";
+import { User } from "src/generated/graphql";
 
 @Component({
     selector: "app-user-details",
@@ -6,7 +11,29 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./user-details.component.scss"]
 })
 export class UserDetailsComponent implements OnInit {
-    constructor() {}
+    public currentUser: User;
+    public state: PageState = "LOADING";
+    private subscription = new Subject();
 
-    ngOnInit(): void {}
+    constructor(private authenticationService: AuthenticationService) {}
+
+    ngOnInit(): void {
+        this.authenticationService
+            .getUserObservable()
+            .pipe(takeUntil(this.subscription))
+            .subscribe((u) => {
+                if (u == null) {
+                    return;
+                }
+                u.then((user) => {
+                    this.currentUser = user;
+                    this.state = "SUCCESS";
+                }).catch((error) => (this.state = "ERROR"));
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscription.next();
+        this.subscription.complete();
+    }
 }
