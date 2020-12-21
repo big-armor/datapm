77e18edd499ddb32f690be1d2e19cdba08a4d509
fullstackdevs14 <fullstackdevs@outlook.com>
2020-12-21 09:43:41 -0800

    Move catalog details to catalog module

--- frontend/src/app/catalog/catalog.module.ts
+++ frontend/src/app/catalog/catalog.module.ts
@@ -5,10 +5,11 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { SharedModule } from "../shared/shared.module";
 
 import { CatalogRoutingModule } from "./catalog-routing.module";
+import { CatalogDetailsComponent } from "./catalog-details/catalog-details.component";
 import { CatalogComponent } from "./catalog/catalog.component";
 
 @NgModule({
-    declarations: [CatalogComponent],
+    declarations: [CatalogComponent, CatalogDetailsComponent],
     imports: [CommonModule, CatalogRoutingModule, MatProgressSpinnerModule, MatTabsModule, SharedModule]
 })
 export class CatalogModule {}
