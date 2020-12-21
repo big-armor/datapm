89a6958c1e153497f727bbe03a1bed144f5ca58b
fullstackdevs14 <fullstackdevs@outlook.com>
2020-12-21 08:44:49 -0800

    update routing for catalog details and user details

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -17,6 +17,14 @@ const staticRoutes: Route[] = [
             import("./collection-details/collection-details.module").then((m) => m.CollectionDetailsModule)
     },
     {
+        path: "user/:username",
+        loadChildren: () => import("./my-account/my-account.module").then((m) => m.MyAccountModule)
+    },
+    {
+        path: "catalog/:catalogSlug",
+        loadChildren: () => import("./catalog-details/catalog-details.module").then((m) => m.CatalogDetailsModule)
+    },
+    {
         path: "me",
         loadChildren: () => import("./my-account/my-account.module").then((m) => m.MyAccountModule),
         canActivate: [AuthGuard]
