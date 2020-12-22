ad9a11c28f095e1d88b9e5304ee4bf72b095153c
fullstackdevs14 <fullstackdevs@outlook.com>
2020-12-21 16:15:33 -0800

    update routing for user details and catalog

--- frontend/src/app/my-account/my-account-routing.module.ts
+++ frontend/src/app/my-account/my-account-routing.module.ts
@@ -2,38 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { MyAccountComponent } from "./my-account/my-account.component";
-import { DetailsComponent } from "./details/details.component";
-import { PackagesComponent } from "./packages/packages.component";
-import { ActivityComponent } from "./activity/activity.component";
-import { CatalogsComponent } from "./catalogs/catalogs.component";
-import { MyCollectionsComponent } from "./my-collections/my-collections.component";
 
 const routes: Routes = [
     {
         path: "",
-        component: MyAccountComponent,
-        children: [
-            {
-                path: "details",
-                component: DetailsComponent
-            },
-            {
-                path: "packages",
-                component: PackagesComponent
-            },
-            {
-                path: "collections",
-                component: MyCollectionsComponent
-            },
-            {
-                path: "catalogs",
-                component: CatalogsComponent
-            },
-            {
-                path: "**",
-                redirectTo: "details"
-            }
-        ]
+        component: MyAccountComponent
     }
 ];
 
