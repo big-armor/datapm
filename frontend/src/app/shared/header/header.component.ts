67b54e1fe1ef8fbfef12a6c4bccf47d675ee2fee
Bryan Szendel <bryan@bigarmor.com>
2020-12-29 15:48:18 -0500

    wip: autocomplete results in search

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 import { FormControl } from "@angular/forms";
-import { takeUntil } from "rxjs/operators";
-import { Subject } from "rxjs";
+import { takeUntil, startWith, map, filter } from "rxjs/operators";
+import { Subject, Observable, BehaviorSubject } from "rxjs";
 
 import { AuthenticationService } from "../../services/authentication.service";
 import { DialogService } from "../../services/dialog.service";
-import { User } from "src/generated/graphql";
+import { AutoCompleteGQL, AutoCompleteResult, User } from "src/generated/graphql";
 import { MatDialog } from "@angular/material/dialog";
 import { LoginDialogComponent } from "./login-dialog/login-dialog.component";
 import { SignUpDialogComponent } from "./sign-up-dialog/sign-up-dialog.component";
@@ -18,6 +18,11 @@ enum State {
     ERROR,
     SUCCESS,
     ERROR_NOT_UNIQUE
+}
+
+interface Option {
+    package?: string;
+    collection?: string;
 }
 @Component({
     selector: "sd-header",
@@ -31,17 +36,32 @@ export class HeaderComponent implements OnInit, OnDestroy {
     searchControl: FormControl;
     private subscription = new Subject();
 
+    autoCompleteResult: AutoCompleteResult;
+
     constructor(
         private matDialog: MatDialog,
         private dialog: DialogService,
         private router: Router,
         private route: ActivatedRoute,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private autocomplete: AutoCompleteGQL
     ) {
         this.searchControl = new FormControl("");
     }
 
     ngOnInit(): void {
+        // this.options = [
+        //     { package: "bryan", collection: "test" },
+        //     { package: "bryan2", collection: "test2" }
+        // ];
+        // this.options = this.searchControl.valueChanges.subscribe(
+        //     // startWith(""),
+        //     value => {
+
+        //         value.map((val) => (val.length >= 1 ? this.autoCompleteFilter(val) : []))
+        //     }
+        // );
+
         this.route.queryParamMap.pipe(takeUntil(this.subscription)).subscribe((queryParams: ParamMap) => {
             this.searchControl.setValue(queryParams.get("q") || "");
         });
@@ -82,6 +102,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
         });
     }
 
+    autoComplete(val: string) {
+        this.autocomplete.fetch({ startsWith: val }).subscribe(({ data }) => {
+            if (!data) return;
+            this.autoCompleteResult = data.autoComplete;
+        });
+    }
+
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
     }
@@ -98,6 +125,23 @@ export class HeaderComponent implements OnInit, OnDestroy {
         console.log(this.searchControl.value);
         this.router.navigate(["/search"], { queryParams: { q: this.searchControl.value } });
     }
+
+    // autoCompleteFilter(val: string) {
+    //     return this.options.pipe(
+    //         map((item) => {
+    //             item.filter((again) => {
+    //                 again.collection.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) === 0 ||
+    //                     again.package.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) === 0;
+    //             });
+    //         })
+    //     );
+    // }
+    // autoCompleteFilter(val: string) {
+    //     return this.options.filter((option) => {
+    //         option.collection.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) === 0 ||
+    //             option.package.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) === 0;
+    //     });
+    // }
 
     goHome() {
         this.router.navigate(["/"]);
