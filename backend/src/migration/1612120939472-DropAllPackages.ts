bf5c2ad8290193913251044168a084c9401add03
Travis Collins <travis@dreamingwell.com>
2021-02-02 14:46:22 -0500

    Remove drop all packages migration from this branch

--- backend/src/migration/1612120939472-DropAllPackages.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class DropAllPackages1612120939472 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        return queryRunner.query("Delete from package");
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
