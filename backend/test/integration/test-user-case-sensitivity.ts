9ba42125779df4cf18180833591a5663b26552a1
Travis Collins <travis@dreamingwell.com>
2021-03-23 21:08:14 -0400

    User emailAddress and username should be case insensitive

---
+++ backend/test/integration/test-user-case-sensitivity.ts
@@ -0,0 +1,46 @@
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client/core";
+import { expect } from "chai";
+import { AdminHolder } from "./admin-holder";
+import { createUser } from "./test-utils";
+
+describe("User Tests", async () => {
+    let userAClient: ApolloClient<NormalizedCacheObject>;
+    let userBClient: ApolloClient<NormalizedCacheObject>;
+
+    before(async () => {});
+
+    it("Create user", async function () {
+        userAClient = await createUser("FirstA", "LastA", "testA-case", "testA-case@test.datapm.io", "passwordA!");
+        AdminHolder.adminClient = userAClient;
+
+        expect(userAClient).to.exist;
+    });
+
+    it("Do not allow a new user with the same email in different case", async function () {
+        let errorFound: any | null = null;
+
+        try {
+            await createUser("FirstA", "LastA", "testA-case2", "testa-case@test.datapm.io", "passwordA!");
+        } catch (error) {
+            errorFound = error;
+        }
+
+        expect(errorFound).to.not.equal(null);
+
+        expect(errorFound?.errors[0].message).to.include("EMAIL_ADDRESS_NOT_AVAILABLE");
+    });
+
+    it("Do not allow a new user with the same username in different case", async function () {
+        let errorFound: any | null = null;
+
+        try {
+            await createUser("FirstA", "LastA", "testa-case", "testa-case2@test.datapm.io", "passwordA!");
+        } catch (error) {
+            errorFound = error;
+        }
+
+        expect(errorFound).to.not.equal(null);
+
+        expect(errorFound?.errors[0].message).to.include("USERNAME_NOT_AVAILABLE");
+    });
+});
