dacb23867dc86719d1b496c8250b8b3f9f9ece4f
Ermal Ferati <ermalferati@gmail.com>
2021-03-26 22:12:50 +0100

    ft/DPM-268 - Add show more sections for package info

--- frontend/src/app/package/components/package-description/package-description.component.ts
+++ frontend/src/app/package/components/package-description/package-description.component.ts
@@ -1,36 +1,60 @@
 import { Component } from "@angular/core";
-import { PackageFile, parsePackageFileJSON, validatePackageFile, validatePackageFileInBrowser } from "datapm-lib";
+import { PackageFile, parsePackageFileJSON, validatePackageFileInBrowser } from "datapm-lib";
 import { Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 import { Package } from "src/generated/graphql";
 import { PackageService, PackageResponse } from "../../services/package.service";
-
 @Component({
     selector: "package-description",
     templateUrl: "./package-description.component.html",
     styleUrls: ["./package-description.component.scss"]
 })
 export class PackageDescriptionComponent {
+    private readonly SHOW_MORE_CHARACTER_LIMIT = 100;
+
     public package: Package;
     public packageFile: PackageFile;
     private unsubscribe$ = new Subject();
-    isShowMore = true
+
+    public shouldShowMoreDescriptionButton: boolean;
+    public isShowingMoreDescriptionText: boolean;
+
+    public shouldShowMoreLicenseButton: boolean;
+    public isShowingMoreLicenseText: boolean;
+
+    public shouldShowMoreReadMeButton: boolean;
+    public isShowingMoreReadMeText: boolean;
 
     constructor(private packageService: PackageService) {
         this.packageService.package.pipe(takeUntil(this.unsubscribe$)).subscribe((p: PackageResponse) => {
             if (p == null || p.package == null) return;
 
             this.package = p.package;
+            this.shouldShowMoreDescriptionButton = this.package.description?.length > this.SHOW_MORE_CHARACTER_LIMIT;
 
-            validatePackageFileInBrowser(p.package.latestVersion.packageFile);
-            this.packageFile = parsePackageFileJSON(p.package.latestVersion.packageFile);
+            // TEMP: DELETE THIS
+            this.packageFile = new PackageFile();
+            this.packageFile.readmeMarkdown = this.package.description;
+            this.packageFile.licenseMarkdown = this.package.description;
+
+            this.shouldShowMoreReadMeButton = this.package.description?.length > this.SHOW_MORE_CHARACTER_LIMIT;
+            this.shouldShowMoreLicenseButton = this.package.description?.length > this.SHOW_MORE_CHARACTER_LIMIT;
+
+            // validatePackageFileInBrowser(p.package.latestVersion.packageFile);
+            // this.packageFile = parsePackageFileJSON(p.package.latestVersion.packageFile);
         });
     }
 
+    public toggleShowMoreDescriptionText() {
+        this.isShowingMoreDescriptionText = !this.isShowingMoreDescriptionText;
+    }
 
+    public toggleShowMoreReadMeText() {
+        this.isShowingMoreReadMeText = !this.isShowingMoreReadMeText;
+    }
 
-    showText() {
-       this.isShowMore = !this.isShowMore
+    public toggleShowMoreLicenseText() {
+        this.isShowingMoreLicenseText = !this.isShowingMoreLicenseText;
     }
 
     ngOnDestroy(): void {
