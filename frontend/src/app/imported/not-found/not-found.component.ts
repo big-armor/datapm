502c3b2fb0198353d40a41a5d8ce96b3729c82d4
Ermal Ferati <ermalferati@gmail.com>
2021-04-14 21:21:44 +0200

    chore/DPM-286 - Add builder io example components

--- frontend/src/app/imported/not-found/not-found.component.ts
+++ frontend/src/app/imported/not-found/not-found.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ResourceImporterService } from "../resource-importer.service";
+import { combineLatest, Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Component({
     selector: "app-not-found",
@@ -8,26 +10,36 @@ import { ResourceImporterService } from "../resource-importer.service";
 })
 export class NotFoundComponent implements OnInit {
     private readonly PAGE_STATIC_FILE_NAME = "404";
+    private readonly PAGE_SCRIPT_STATIC_FILE_NAME = "header";
 
     public html: string;
+    public javascript: string;
     public loading: boolean = false;
 
     constructor(private resourceImporterService: ResourceImporterService) {}
 
     public ngOnInit(): void {
-        this.loadExternalHtml();
+        this.loadExternalContent();
     }
 
-    private loadExternalHtml(): void {
+    private loadExternalContent(): void {
         this.loading = true;
-        this.resourceImporterService.getHtml(this.PAGE_STATIC_FILE_NAME).subscribe(
-            (html) => {
-                this.html = html;
-                this.loading = false;
-            },
-            () => {
-                this.loading = false;
-            }
+        combineLatest([this.loadExternalHtml(), this.loadExternalJavascript()]).subscribe(
+            ([html, js]) => (this.loading = false),
+            () => (this.loading = false)
+        );
+    }
+
+    private loadExternalHtml(): Observable<string> {
+        return this.resourceImporterService.getHtml(this.PAGE_STATIC_FILE_NAME).pipe(tap((html) => (this.html = html)));
+    }
+
+    private loadExternalJavascript(): Observable<string> {
+        return this.resourceImporterService.getJavascript(this.PAGE_SCRIPT_STATIC_FILE_NAME).pipe(
+            tap((javascript) => {
+                this.javascript = javascript;
+                eval(this.javascript);
+            })
         );
     }
 }
