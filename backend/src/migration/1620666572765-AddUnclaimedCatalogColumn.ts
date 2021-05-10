7e8ab43b634910fd7ee0c472bc374e1b4e1d459a
Ermal Ferati <ermalferati@gmail.com>
2021-05-10 22:29:29 +0200

    ft/DPM-269 - Add catalog unclaimed status

---
+++ backend/src/migration/1620666572765-AddUnclaimedCatalogColumn.ts
@@ -0,0 +1,10 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUnclaimedCatalogColumn1620666572765 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const sql = "UPDATE TABLE 'catalog' ADD COLUMN IF NOT EXISTS 'unclaimed' BOOLEAN DEFAULT FALSE";
+        return queryRunner.query(sql);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
