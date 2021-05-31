df8f4ec6a84d1ca92addeaff7c494a271929a52c
Ermal Ferati <ermalferati@gmail.com>
2021-05-31 16:57:36 +0200

    chore/DPM - Fix lint attempt 3

--- backend/src/resolvers/CatalogResolver.ts
+++ backend/src/resolvers/CatalogResolver.ts
@@ -30,7 +30,6 @@ export const catalogEntityToGraphQLOrNull = (catalogEntity: CatalogEntity): Cata
     if (!catalogEntity) {
         return null;
     }
-
     return catalogEntityToGraphQL(catalogEntity);
 };
 
