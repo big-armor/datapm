b65a01c2e22ba6c675cb0d4d0334b930f01ac315
Ermal Ferati <ermalferati@gmail.com>
2021-06-22 21:35:04 +0200

    chore/DPM-339 - Adjust migration after recent notifications changes

--- backend/src/migration/1624308986637-AddPackageIssueForeignKeyToLogsTable.ts
+++ backend/src/migration/1624308986637-AddPackageIssueForeignKeyToLogsTable.ts
@@ -2,22 +2,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddPackageIssueForeignKeyToLogsTable1624308986637 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const query = `
-        DELETE FROM activity_log
-        WHERE target_package_issue_id IS NOT NULL
-        AND NOT EXISTS(
-                SELECT id
-                FROM package_issue
-                WHERE id = target_package_issue_id
-            );
-
-        ALTER TABLE activity_log
-        ADD CONSTRAINT activity_log_target_package_issue_id_fkey
-        FOREIGN KEY (target_package_issue_id)
-        REFERENCES package_issue(id)
-        ON DELETE CASCADE;`;
-        return queryRunner.query(query);
+        queryRunner.connection.query("DELETE FROM activity_log WHERE event_type = 'PACKAGE_ISSUE_STAUS_CHANGE'");
+        queryRunner.connection.query("ALTER TYPE activity_log_change_type_enum ADD VALUE 'OPENED'");
+        queryRunner.connection.query("ALTER TYPE activity_log_change_type_enum ADD VALUE 'CLOSED'");
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {}
+    public async down(queryRunner: QueryRunner): Promise<void> { }
 }
