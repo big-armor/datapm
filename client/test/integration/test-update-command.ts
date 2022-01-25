00671f0d531d5905fcc45b8f5da5cd43708db16d
Travis Collins <travis@dreamingwell.com>
2022-01-24 21:44:40 -0500

    Separated update functionality into edit command

--- client/test/integration/test-update-command.ts
+++ client/test/integration/test-update-command.ts
@@ -20,9 +20,7 @@ import {
     removePackageFiles,
     testCmd,
     writeCSVFile,
-    TestResults,
-    KEYS,
-    TEST_SOURCE_FILES
+    TestResults
 } from "./test-utils";
 
 // Prompts
@@ -31,15 +29,7 @@ const publishCommandPrompts = ["Target registry?", "Catalog short name?", "Data 
 const getPublishCommandPromptInputs = (inputs?: string[], skip = 0, count = 20) =>
     getPromptInputs(publishCommandPrompts, inputs, skip, count);
 
-const updateCommandPrompts = [
-    "Exclude any attributes",
-    "Rename attributes",
-    "User friendly package name?",
-    "Next version?",
-    "Short package description?",
-    "Website?",
-    "Number of sample records?"
-];
+const updateCommandPrompts: string[] = [];
 
 const getUpdateCommandPromptInputs = (inputs?: string[], skip = 0, count = 20) =>
     getPromptInputs(updateCommandPrompts, inputs, skip, count);
@@ -180,39 +170,7 @@ describe("Update Package Command Tests", async () => {
     });
 
     it("Should overwrite the existing package file", async () => {
-        const prompts = getUpdateCommandPromptInputs([
-            "",
-            "",
-            "updated package 100",
-            "",
-            "Updated Package 100",
-            "https://website100.com",
-            "10"
-        ]);
-        prompts.splice(
-            2,
-            0,
-            {
-                message: "What does each test record represent?",
-                input: `unit${KEYS.ENTER}`
-            },
-            {
-                message: "Do you want to edit units for the ",
-                input: "y" + KEYS.ENTER
-            },
-            {
-                message: "Unit for attribute 'Random integer'?",
-                input: `integer${KEYS.ENTER}`
-            },
-            {
-                message: "Unit for attribute 'Random float'?",
-                input: `float${KEYS.ENTER}`
-            },
-            {
-                message: "Unit for attribute 'Increasing series integer'?",
-                input: `integer serie${KEYS.ENTER}`
-            }
-        );
+        const prompts = getUpdateCommandPromptInputs();
         const results: TestResults = {
             exitCode: -1,
             messageFound: false
@@ -233,58 +191,27 @@ describe("Update Package Command Tests", async () => {
 
         expect(cmdResult.code, "Exit code").equals(0);
         expect(results.messageFound, "Found success message").equals(true);
-        expect(newPackageFile.displayName).to.be.string("updated package 100");
+        expect(newPackageFile.displayName).to.be.string("test");
         expect(newPackageFile.packageSlug).to.be.string("test");
-        expect(newPackageFile.version).to.be.string("1.0.1");
-        expect(newPackageFile.description).to.be.string("Updated Package 100");
-        expect(newPackageFile.website).to.be.string("https://website100.com");
-        expect(newPackageFile.schemas[0].sampleRecords?.length).equals(10);
+        expect(newPackageFile.version).to.be.string("1.0.0");
+        expect(newPackageFile.description).to.be.string("Generated from file://./test.csv");
+        expect(newPackageFile.schemas[0].sampleRecords?.length).equals(100);
         expect(newPackageFile.schemas[0].recordCount).equals(150);
-        expect(newPackageFile.schemas[0].unit).equals("unit");
+        expect(newPackageFile.schemas[0].unit).equals(undefined);
         const properties = newPackageFile.schemas[0].properties as Properties;
-        expect(properties["Random integer"].unit).equals("integer");
-        expect(properties["Random float"].unit).equals("float");
-        expect(properties["Increasing series integer"].unit).equals("integer serie");
+        expect(properties["Random integer"].unit).equals(undefined);
+        expect(properties["Random float"].unit).equals(undefined);
+        expect(properties["Increasing series integer"].unit).equals(undefined);
     });
 
     it("Should update the package file based on the updated source file", async () => {
         updateSourceFile("test.csv");
 
-        const prompts = getUpdateCommandPromptInputs([
-            "",
-            "",
-            "updated package 200",
-            "",
-            "Updated Package 200",
-            "https://website200.com",
-            "10"
-        ]);
-        prompts.splice(
-            2,
-            0,
-            {
-                message: "What does each test record represent?",
-                input: KEYS.ENTER
-            },
-            {
-                message: "Do you want to edit units",
-                input: `y${KEYS.ENTER}`
-            },
-            {
-                message: "Unit for attribute 'Random integer'?",
-                input: KEYS.ENTER
-            },
-            {
-                message: "Unit for attribute 'Changed random float'?",
-                input: KEYS.ENTER
-            }
-        );
+        const prompts = getUpdateCommandPromptInputs();
         const results: TestResults = {
             exitCode: -1,
             messageFound: false
         };
-
-        const oldPackageFile: PackageFile = loadPackageFileFromDisk("test.datapm.json");
 
         const cmdResult = await testCmd(
             "update",
@@ -301,78 +228,14 @@ describe("Update Package Command Tests", async () => {
 
         expect(cmdResult.code, "Exit code").equals(0);
         expect(results.messageFound, "Found success message").equals(true);
-        expect(newPackageFile.displayName).be.string("updated package 200");
+        expect(newPackageFile.displayName).be.string("test");
         expect(newPackageFile.packageSlug).be.string("test");
         expect(newPackageFile.version).be.string("2.0.0");
-        expect(newPackageFile.description).be.string("Updated Package 200");
-        expect(newPackageFile.website).be.string("https://website200.com");
-        expect(newPackageFile.schemas[0].sampleRecords?.length).equals(10);
+        expect(newPackageFile.description).be.string("Generated from file://./test.csv");
+        expect(newPackageFile.website).be.string("");
+        expect(newPackageFile.schemas[0].sampleRecords?.length).equals(100);
         expect(newPackageFile.schemas[0].recordCount).equals(200);
-        expect(newPackageFile.schemas[0].unit).equals("unit");
-        const properties = newPackageFile.schemas[0].properties as Properties;
-        expect(properties["Random integer"].unit).equals("integer");
-        expect(properties["Changed random float"].unit).equals(undefined);
-
-        // test presence of added column
-        expect(Object.keys(oldPackageFile.schemas[0].properties || {}).indexOf("Changed random float") === -1).equal(
-            true
-        );
-        expect(Object.keys(newPackageFile.schemas[0].properties || {}).indexOf("Changed random float") !== -1).equal(
-            true
-        );
-
-        // Test removed column
-        expect(Object.keys(oldPackageFile.schemas[0].properties || {}).indexOf("Increasing series dates") !== -1).equal(
-            true
-        );
-        expect(Object.keys(newPackageFile.schemas[0].properties || {}).indexOf("Increasing series dates") === -1).equal(
-            true
-        );
-    });
-
-    it("Should confirm if there are more than 10 number type properties", async function () {
-        await createTestPackage(TEST_SOURCE_FILES.FILE6, true);
-
-        const prompts = getUpdateCommandPromptInputs([
-            "",
-            "",
-            "updated package 200",
-            "",
-            "Updated Package 200",
-            "https://website200.com",
-            "10"
-        ]);
-        prompts.splice(
-            2,
-            0,
-            {
-                message: "What does each us-covid record represent?",
-                input: `unit${KEYS.ENTER}`
-            },
-            {
-                message: "Do you want to edit units",
-                input: `n${KEYS.ENTER}`
-            }
-        );
-        const results: TestResults = {
-            exitCode: -1,
-            messageFound: false
-        };
-
-        const cmdResult = await testCmd(
-            "update",
-            ["us-covid.datapm.json", "--forceUpdate"],
-            prompts,
-            async (line: string) => {
-                if (line.includes("When you are ready, you can publish with the following command")) {
-                    results.messageFound = true;
-                }
-            }
-        );
-
-        expect(cmdResult.code, "Exit code").equals(0);
-        expect(results.messageFound, "Found success message").equals(true);
-        removePackageFiles(["us-covid"]);
+        expect(newPackageFile.schemas[0].unit).equals(undefined);
     });
 
     it("Publish package A", async () => {
@@ -422,28 +285,7 @@ describe("Update Package Command Tests", async () => {
             exitCode: -1,
             messageFound: false
         };
-        const prompts = getUpdateCommandPromptInputs([
-            "",
-            "",
-            "updated package 101",
-            "",
-            "Updated Package 101",
-            "https://website100.com",
-            "10"
-        ]);
-
-        prompts.splice(
-            2,
-            0,
-            {
-                message: "What does each us-covid record represent?",
-                input: `${KEYS.ENTER}`
-            },
-            {
-                message: "Do you want to edit units",
-                input: `n${KEYS.ENTER}`
-            }
-        );
+        const prompts = getUpdateCommandPromptInputs();
 
         const cmdResult = await testCmd(
             "update",
@@ -533,130 +375,5 @@ describe("Update Package Command Tests", async () => {
 
         expect(cmdResult.code, "Exit code").equals(1);
         expect(results.messageFound, "Found error message").equals(true);
-    });
-
-    it("Generate package with excluding and renaming attributes", async function () {
-        const generateCommandPrompts = [
-            "Is there a header line above?",
-            "Header row line number?",
-            "How are files updated?",
-            "Exclude any attributes",
-            "Attributes to exclude?",
-            "Rename attributes",
-            "Attributes to rename?",
-            `New attribute name for "State Name"?`,
-            "derived from other 'upstream data'?",
-            "What does each state-codes record represent?",
-            "User friendly package name?",
-            "Package short name?",
-            "Starting version?",
-            "Short package description?",
-            "Website?",
-            "Number of sample records?",
-            "Publish to registry?"
-        ];
-        const promptInputs = getPromptInputs(generateCommandPrompts, [
-            "",
-            "0",
-            "",
-            "Y",
-            `${KEYS.DOWN}${KEYS.DOWN} `,
-            "Y",
-            `${KEYS.DOWN} `,
-            "New State Name",
-            "",
-            "",
-            "Package B",
-            "package-b",
-            "",
-            "This is a short description",
-            "https://old-website",
-            "10", // number of records
-            "no" // publish to registry
-        ]);
-        const results: TestResults = {
-            exitCode: -1,
-            messageFound: false
-        };
-
-        const cmdResult = await testCmd("package", [TEST_SOURCE_FILES.HTTP1], promptInputs, async (line: string) => {
-            if (line.includes("When you are ready, you can publish with the following command")) {
-                results.messageFound = true;
-            }
-        });
-
-        expect(cmdResult.code, "Exit code").equals(0);
-        expect(results.messageFound, "Found success message").equals(true);
-    });
-
-    it("Should honor the excluded and renamed attributes", async () => {
-        const prompts = [
-            { message: "Exclude any attributes", input: `y${KEYS.ENTER}` },
-            { message: "Attributes to exclude?", input: ` ${KEYS.ENTER}` },
-            { message: "Rename attributes", input: `y${KEYS.ENTER}` },
-            { message: "Attributes to rename?", input: `${KEYS.ENTER}` },
-            {
-                message: `New attribute name for "New State Name [original: State Name]"?`,
-                input: `New State Name 2${KEYS.ENTER}`
-            },
-            {
-                message: `What does each state-codes record represent?`,
-                input: `unit2${KEYS.ENTER}`
-            },
-            {
-                message: `User friendly package name?`,
-                input: `updated package b${KEYS.ENTER}`
-            },
-            {
-                message: `Next version?`,
-                input: `${KEYS.ENTER}`
-            },
-            {
-                message: `Short package description?`,
-                input: `Updated Package b${KEYS.ENTER}`
-            },
-            {
-                message: `Website?`,
-                input: `https://website-b.com${KEYS.ENTER}`
-            },
-            {
-                message: `Number of sample records?`,
-                input: `9${KEYS.ENTER}`
-            }
-        ];
-
-        const results: TestResults = {
-            exitCode: -1,
-            messageFound: false
-        };
-
-        const cmdResult = await testCmd(
-            "update",
-            ["package-b.datapm.json", "--forceUpdate"],
-            prompts,
-            async (line: string) => {
-                if (line.includes("When you are ready, you can publish with the following command")) {
-                    results.messageFound = true;
-                }
-            }
-        );
-
-        const newPackageFile: PackageFile = loadPackageFileFromDisk("package-b.datapm.json");
-
-        expect(cmdResult.code, "Exit code").equals(0);
-        expect(results.messageFound, "Found success message").equals(true);
-        expect(newPackageFile.displayName).to.be.string("updated package b");
-        expect(newPackageFile.packageSlug).to.be.string("package-b");
-        expect(newPackageFile.version).to.be.string("1.1.0");
-        expect(newPackageFile.description).to.be.string("Updated Package b");
-        expect(newPackageFile.website).to.be.string("https://website-b.com");
-        expect(newPackageFile.schemas[0].sampleRecords?.length).equals(9);
-        expect(newPackageFile.schemas[0].unit).equals("unit2");
-
-        const properties = newPackageFile.schemas[0].properties as Properties;
-
-        expect(properties["State Code"].hidden).equal(false);
-        expect(properties["State Name"].title).equals("New State Name 2");
-        expect(Object.keys(properties).length).equals(2);
     });
 });
