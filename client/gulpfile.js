048a8f7467eadd5270ffea24f20226e7a107bce0
Travis Collins <travis@dreamingwell.com>
2022-02-03 14:21:32 -0500

    Separated packaging, signing, and bundling

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -91,62 +91,70 @@ function spawnAndLog(prefix, command, args, opts) {
 /** Windows 64 Bit */
 
 function runPkgWin64() {
-    return runPkg("win", "x64", "pkg-win64");
+    return runPkg("win", "x64", "pkg-windows-intel64");
 }
 
 function cleanWin64() {
     if (fs.existsSync("datapm-client-" + readPackageVersion() + "-x64.msix"))
         del("datapm-client-" + readPackageVersion() + "-x64.msix");
 
-    return cleanDir("pkg-win64");
+    return cleanDir("pkg-windows-intel64");
 }
 
 function copyDepsWin64() {
-    return copyDeps("pkg-win64");
+    return copyDeps("pkg-windows-intel64");
 }
 
 function copyAssetsWin64() {
-    return copyAssets("pkg-win64");
+    return copyAssets("pkg-windows-intel64");
 }
 
 function copyAppManifiestWin64() {
-    return copyAppManfifest("pkg-win64");
+    return copyAppManfifest("pkg-windows-intel64");
 }
 
 function createMsiWin64() {
-    return spawnAndLog("msi-win64", "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\makeappx.exe", [
-        "pack",
-        "/d",
-        "pkg-win64",
-        "/p",
-        "installers/windows/dist/datapm-client-" + readPackageVersion() + "-x64.msix"
-    ]);
+    return spawnAndLog(
+        "msi-intel64",
+        "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\makeappx.exe",
+        [
+            "pack",
+            "/d",
+            "pkg-windows-intel64",
+            "/p",
+            "installers/windows/dist/datapm-client-" + readPackageVersion() + "-x64.msix"
+        ]
+    );
 }
 
 function signMsiWin64() {
-    return spawnAndLog("sign-win64", "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\signtool.exe", [
-        "sign",
-        "/fd",
-        "SHA256",
-        "/a",
-        "/f",
-        "signing-certificate.pfx",
-        "/p",
-        process.env.CERTIFICATE_PASSWORD,
-        "installers/windows/dist/datapm-client-" + readPackageVersion() + "-x64.msix"
-    ]);
+    return spawnAndLog(
+        "sign-intel64",
+        "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\signtool.exe",
+        [
+            "sign",
+            "/fd",
+            "SHA256",
+            "/a",
+            "/f",
+            "signing-certificate.pfx",
+            "/p",
+            process.env.CERTIFICATE_PASSWORD,
+            "installers/windows/dist/datapm-client-" + readPackageVersion() + "-x64.msix"
+        ]
+    );
 }
 
 /** Windows arm64 */
 
-function runPkgArm64() {
-    return runPkg("win", "arm64", "pkg-arm64");
+function runPkgWindowsArm64() {
+    return runPkg("win", "arm64", "pkg-windows-arm64");
 }
 
-function cleanArm64() {
+function cleanWindowsArm64() {
     if (fs.existsSync("datapm-client-" + readPackageVersion() + "-arm64.msix"))
         del("datapm-client-" + readPackageVersion() + "-arm64.msix");
-    return cleanDir("pkg-arm64");
+    return cleanDir("pkg-windows-arm64");
 }
 
 function cleanMacOSInstaller() {
@@ -155,36 +163,34 @@ function cleanMacOSInstaller() {
 }
 
 function writeCertificateFile() {
-    fs.writeFileSync("signing-certificate.pfx", process.env.CERTIFICATE_BASE64, { encoding: "base64" }, function () {
-        console.log("Signing Certificate");
-    });
+    fs.writeFileSync("signing-certificate.pfx", process.env.CERTIFICATE_BASE64, { encoding: "base64" });
 
     return Promise.resolve();
 }
 
-function copyDepsArm64() {
-    return copyDeps("pkg-arm64");
+function copyDepsWindowsArm64() {
+    return copyDeps("pkg-windows-arm64");
 }
 
-function copyAssetsArm64() {
-    return copyAssets("pkg-arm64");
+function copyAssetsWindowsArm64() {
+    return copyAssets("pkg-windows-arm64");
 }
 
-function copyAppManifiestArm64() {
-    return copyAppManfifest("pkg-arm64");
+function copyAppManifiestWindowsArm64() {
+    return copyAppManfifest("pkg-windows-arm64");
 }
 
-function createMsiArm64() {
+function createMsiWindowsArm64() {
     return spawnAndLog("msi-arm64", "makeappx.exe", [
         "pack",
         "/d",
-        "pkg-arm64",
+        "pkg-windows-arm64",
         "/p",
         "installers/windows/dist/datapm-client-" + readPackageVersion() + "-arm64.msix"
     ]);
 }
 
-function signMsiArm64() {
+function signMsiWindowsArm64() {
     return spawnAndLog("sign-arm64", "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\signtool.exe", [
         "sign",
         "/fd",
@@ -199,36 +205,36 @@ function signMsiArm64() {
 }
 /** Mac 64 Bit Intel */
 function runPkgMacIntel64() {
-    return runPkg("macos", "x64", "pkg-mac64");
+    return runPkg("macos", "x64", "pkg-mac-intel64");
 }
 
 function cleanMacIntel64() {
-    return cleanDir("pkg-mac64");
+    return cleanDir("pkg-mac-intel64");
 }
 
 function copyDepsMacIntel64() {
-    return copyDeps("pkg-mac64");
+    return copyDeps("pkg-mac-intel64");
 }
 
 function copyAssetsMacIntel64() {
-    return copyAssets("pkg-mac64");
+    return copyAssets("pkg-mac-intel64");
 }
 
 /** Arm 64 Bit */
 function runPkgMacArm64() {
-    return runPkg("macos", "arm64", "pkg-arm64");
+    return runPkg("macos", "arm64", "pkg-mac-arm64");
 }
 
 function cleanMacArm64() {
-    return cleanDir("pkg-arm64");
+    return cleanDir("pkg-mac-arm64");
 }
 
 function copyDepsMacArm64() {
-    return copyDeps("pkg-arm64");
+    return copyDeps("pkg-mac-arm64");
 }
 
 function copyAssetsMacArm64() {
-    return copyAssets("pkg-arm64");
+    return copyAssets("pkg-mac-arm64");
 }
 
 function cleanDist() {
@@ -277,23 +283,24 @@ function signWinBundle() {
 }
 
 exports.buildWindows = series(
-    cleanWin64,
     writeCertificateFile,
+    cleanWin64,
     runPkgWin64,
     copyDepsWin64,
     copyAssetsWin64,
     copyAppManifiestWin64,
+    cleanWindowsArm64,
+    runPkgWindowsArm64,
+    copyDepsWindowsArm64,
+    copyAssetsWindowsArm64,
+    copyAppManifiestWindowsArm64
+);
+
+exports.signAndBundleWindows = series(
     createMsiWin64,
     signMsiWin64,
-    /* 
-    cleanArm64
-    writeCertificateFile,
-    runPkgArm64,
-    copyDepsArm64,
-    copyAssetsArm64,
-    copyAppManifiestArm64,
-    createMsiArm64,
-    signMsiArm64, */
+    createMsiWindowsArm64,
+    signMsiWindowsArm64,
     bundleWinInstallers,
     signWinBundle
 );
@@ -301,15 +308,15 @@ exports.buildWindows = series(
 exports.buildWindowsBundle = series(bundleWinInstallers, signWinBundle);
 
 exports.buildMacOS = series(
-    /* cleanMacArm64,
+    cleanMacArm64,
     runPkgMacArm64,
     copyDepsMacArm64,
-    copyAssetsMacArm64, */
+    copyAssetsMacArm64,
     cleanMacIntel64,
     runPkgMacIntel64,
     copyDepsMacIntel64,
     copyAssetsMacIntel64
 );
-exports.clean = series(cleanDist, cleanMacIntel64, cleanWin64, cleanArm64, cleanMacOSInstaller);
+exports.clean = series(cleanDist, cleanMacIntel64, cleanWin64, cleanWindowsArm64, cleanMacOSInstaller);
 
 exports.postCodegen = postCodegen;
