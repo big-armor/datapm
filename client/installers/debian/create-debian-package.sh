7ad62eb3eba727cec674a957e56d2ee90faaaf7d
Travis Collins <travis@dreamingwell.com>
2022-02-07 16:54:32 -0500

    back to caps

--- client/installers/debian/create-debian-package.sh
+++ client/installers/debian/create-debian-package.sh
@@ -9,15 +9,15 @@ rm -f *.deb || true
 
 # Create directory
 echo "Creating build directory..."
-mkdir -p build/debian/source
+mkdir -p build/DEBIAN/source
 
 # Move files into place
 echo "Copy package files..."
-cp changelog control datapm-client.links datapm-client.install rules build/debian
+cp changelog control datapm-client.links datapm-client.install rules build/DEBIAN
 
 # Move build files into place
 echo "Copy source..."
-cp -R ../../pkg-linux-intel64/* build/debian/source
+cp -R ../../pkg-linux-intel64/* build/debian/DEBIAN
 
 # Extract version number from package.json
 DATAPM_VERSION=`jq .version ../../../package.json  | tr -d '"'`
@@ -25,7 +25,7 @@ echo "DATAPM_VERSION: $DATAPM_VERSION"
 
 # Replace version number in control file
 echo "Replacing datapm version in control file..."
-sed -i "s/x\.x\.x/$DATAPM_VERSION/g" build/debian/changelog
+sed -i "s/x\.x\.x/$DATAPM_VERSION/g" build/DEBIAN/changelog
 
 
 # Build package file
