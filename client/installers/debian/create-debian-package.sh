b677dfa9ccf028ab057b82c445b6a2da54658cc2
Travis Collins <travis@dreamingwell.com>
2022-02-07 12:27:57 -0500

    Replace version number in build file

--- client/installers/debian/create-debian-package.sh
+++ client/installers/debian/create-debian-package.sh
@@ -10,5 +10,11 @@ cp datapm.install postinstall control build/DEBIAN
 # Move build files into place
 cp -R ../../pkg-linux-intel64/* build
 
+# Extract version number from package.json
+DATAPM_VERSION=`jq .version ../../../package.json  | tr -d '"'`
+
+# Replace version number in control file
+sed -i "s/x\.x\.x/$DATAPM_VERSION/g" build/DEBIAN/control
+
 # Build package file
 dpkg-deb --build build
