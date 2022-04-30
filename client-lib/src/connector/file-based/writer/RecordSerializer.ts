7b263a134439f7e6395d5aec03525f96b23e3ccf
Travis Collins <travis@dreamingwell.com>
2022-04-29 22:12:45 -0400

    Added Parser and Serializer documentation

--- client-lib/src/connector/file-based/writer/RecordSerializer.ts
+++ client-lib/src/connector/file-based/writer/RecordSerializer.ts
@@ -4,22 +4,38 @@ import { PackageFile, Schema, DPMConfiguration, UpdateMethod, Parameter } from "
 export interface DPMRecordSerializerDescription {
     getDisplayName(): string;
 
+    /** Return the Mimetype of the file that will be written */
     getOutputMimeType(): string;
 
+    /** Return the extension of the file that will be written. */
     getFileExtension(): string;
 
+    /** Import the DPMRecordSerializer implementation in this method.
+     * Do not import the class outside of this method.
+     */
     getRecordSerializer(): Promise<DPMRecordSerializer>;
 }
 
 export interface DPMRecordSerializer {
+    /** Return the user friendly display name.  */
     getDisplayName(): string;
 
+    /** Whether the file format being written strictly controls the types of
+     * each filed (ex: Avro).
+     */
     isStronglyTyped(configuration: DPMConfiguration): boolean;
 
+    /** Return the Mimetype of the file that will be written */
     getOutputMimeType(): string;
 
+    /** Return the extension of the file that will be written. */
     getFileExtension(): string;
 
+    /** Return the default values for any user options.
+     *
+     * This method may be depreciated in the future. Use the "default" value of the Parameter object
+     * returned in the "getParameters" method instead.
+     */
     getDefaultParameterValues(
         catalogSlug: string | undefined,
         packageFile: PackageFile,
