f39763c5951c7047a9c3ebb70f0da34f0b23b588
Citizen Josh <citizenjosh@users.noreply.github.com>
2023-03-30 12:54:31 -0400

    token in the code

--- backend/src/directive/ValidBase64ImageUploadDirective.ts
+++ backend/src/directive/ValidBase64ImageUploadDirective.ts
@@ -10,7 +10,7 @@ export class ValidBase64ImageUploadDirective extends SchemaDirectiveVisitor {
     private static readonly BASE64_TAG = ";base64,";
     private static readonly BASE64_DATA_TAG = "data:";
 
-public static password = "asdklfjsldkjf45345lkjslfj"; // this is bad
+public static MY_FAKE_GITHUB_TOKEN = "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"; // this is bad
 
     visitArgumentDefinition(
         argument: GraphQLArgument,
